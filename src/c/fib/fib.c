b1d52e451d7eab0fa10be079c4ae87082100e217
Ohashi-Dev <ohashi.developer@gmail.com>
2022-08-10 14:03:08 +0900

    fix

--- src/c/fib/fib.c
+++ src/c/fib/fib.c
@@ -1,11 +1,14 @@
-int fib(int n) {
-  if(n <= 1) return 1;
-  return fib(n-1) + fib(n-2);
+int fib(int n)
+{
+  if (n <= 1)
+    return 1;
+  return fib(n - 1) + fib(n - 2);
 }
 
-int main() {
+int main()
+{
   fib(9);
-  for(;;) {
-  }
+  // for(;;) {
+  // }
   return 0;
 }
